b18f9f7b62a411f0b14818e9b8449aa891f680b7
Arthit3108 <arthit.nop@gmail.com>
2026-03-26 13:00:46 +0700

    chage frontendirl

--- backend/internal/middleware/middleware.go
+++ backend/internal/middleware/middleware.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -8,9 +10,13 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
-func SetupMiddleware(app *fiber.App) {
+	frontendUrl := os.Getenv("FRONTEND_URL")
+	if frontendUrl == "" {
+		frontendUrl = "http://localhost:5173, http://localhost:5174"
+	}
+
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173, http://localhost:5174",
+		AllowOrigins:     frontendUrl,
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, HEAD, PUT, DELETE, PATCH",
 		AllowCredentials: true,
